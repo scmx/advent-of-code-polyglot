f26ca49ae906b42db61296f9f25bebaa97468848
Albert Arvidsson <albert.arvidsson@gmail.com>
2023-12-02 06:34:32 +0100

    Fix test classes to allow running all tests

--- test_helper.rb
+++ test_helper.rb
@@ -1,36 +1,19 @@
 require 'open3'
+require 'minitest/reporters'
 
-class Helper
-  attr_reader :input_path, :solve_path
-
-  def self.instance
-    @instance ||= new
-  end
-
-  private_class_method :new
-
-  def initialize
-    year = ENV.fetch('AOC_YEAR')
-    day = ENV.fetch('AOC_DAY')
-    part = ENV.fetch('AOC_PART')
-
-    day_pretty = "day_#{format '%02d', day}"
-    part_pretty = "part_#{part}"
-
-    self.input_path = "./#{year}/#{day_pretty}/input"
-    self.solve_path = "./#{year}/#{day_pretty}/#{part_pretty}/solve"
-  end
-
-  private
-
-  attr_writer :input_path, :solve_path
-end
+Minitest::Reporters.use!
 
 def input
-  File.read(Helper.instance.input_path)
+  test_path = caller.first.split(':').first
+  year, day = test_path.split('/').slice(-4..-2)
+  input_path = "#{year}/#{day}/input"
+  File.read(input_path)
 end
 
 def solve(input)
-  stdout, _status = Open3.capture2(Helper.instance.solve_path, stdin_data: input)
+  test_path = caller.first.split(':').first
+  year, day, part = test_path.split('/').slice(-4..-2)
+  solve_path = "#{year}/#{day}/#{part}/solve"
+  stdout, _status = Open3.capture2(solve_path, stdin_data: input)
   stdout.chomp
 end
